dc3e19e04503ff7c24e542bb4d8cdc1d5c3254cd
odrevet <olivier.drevet@cycleco.eu>
2017-11-15 15:33:29 +0100

    removed one share of too many

--- utils/rename_app.sh
+++ utils/rename_app.sh
@@ -11,5 +11,5 @@ mv po/fr/$PROJECT_NAME.po po/fr/$NEW_PROJECT_NAME.po
 mv po/fr/$PROJECT_NAME.mo po/fr/$NEW_PROJECT_NAME.mo
 mv $PROJECT_NAME/$PROJECT_NAME.png $PROJECT_NAME/$NEW_PROJECT_NAME.png
 mv $PROJECT_NAME/ui/$PROJECT_NAME.ui $PROJECT_NAME/ui/$NEW_PROJECT_NAME.ui
-mv share/share/glib-2.0/schemas/$PROJECT_NAME.xml share/share/glib-2.0/schemas/$NEW_PROJECT_NAME.xml
+mv share/glib-2.0/schemas/$PROJECT_NAME.xml share/glib-2.0/schemas/$NEW_PROJECT_NAME.xml
 mv $PROJECT_NAME $NEW_PROJECT_NAME
