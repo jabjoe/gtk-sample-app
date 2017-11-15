4ea253c7ef4ee3f8d3e93adc3a8b58f59ae58f8e
odrevet <olivier.drevet@cycleco.eu>
2017-11-15 15:22:23 +0100

    rename content of ui

--- utils/rename_app.sh
+++ utils/rename_app.sh
@@ -5,7 +5,7 @@ normalize() {
 
 PROJECT_NAME="sampleapp"
 NEW_PROJECT_NAME= normalize $1
-sed -i -e "s/$PROJECT_NAME/$NEW_PROJECT_NAME/g" CMakeLists.txt $PROJECT_NAME.desktop
+sed -i -e "s/$PROJECT_NAME/$NEW_PROJECT_NAME/g" CMakeLists.txt $PROJECT_NAME.desktop $PROJECT_NAME/ui/$PROJECT_NAME.ui
 mv po/$PROJECT_NAME.pot po/$NEW_PROJECT_NAME.pot
 mv po/fr/$PROJECT_NAME.po po/fr/$NEW_PROJECT_NAME.po
 mv po/fr/$PROJECT_NAME.mo po/fr/$NEW_PROJECT_NAME.mo
