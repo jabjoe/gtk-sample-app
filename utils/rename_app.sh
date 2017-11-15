7d36ab25ea661923660cdf1c82294a6e19d6051b
odrevet <olivier.drevet@cycleco.eu>
2017-11-15 15:13:10 +0100

    rename schenma, normalize input

--- utils/rename_app.sh
+++ utils/rename_app.sh
@@ -1,9 +1,15 @@
+normalize() {
+    local safe=${1//[^[:alnum:]]/}
+    echo "${safe,,}"
+}
+
 PROJECT_NAME="sampleapp"
-NEW_PROJECT_NAME=$1
+NEW_PROJECT_NAME= normalize $1
 sed -i -e "s/$PROJECT_NAME/$NEW_PROJECT_NAME/g" CMakeLists.txt $PROJECT_NAME.desktop
 mv po/$PROJECT_NAME.pot po/$NEW_PROJECT_NAME.pot
 mv po/fr/$PROJECT_NAME.po po/fr/$NEW_PROJECT_NAME.po
 mv po/fr/$PROJECT_NAME.mo po/fr/$NEW_PROJECT_NAME.mo
 mv $PROJECT_NAME/$PROJECT_NAME.png $PROJECT_NAME/NEW_PROJECT_NAME.png
 mv $PROJECT_NAME/ui/$PROJECT_NAME.ui $PROJECT_NAME/ui/NEW_PROJECT_NAME.ui
+mv share/share/glib-2.0/schemas/$PROJECT_NAME.xml share/share/glib-2.0/schemas/$NEW_PROJECT_NAME.xml
 mv $PROJECT_NAME $NEW_PROJECT_NAME
